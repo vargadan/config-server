94a758307cf40864feff06b72ae910acf5ede739
vargadan <danielvrg@gmail.com>
2017-09-18 01:11:53 +0200

    pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -12,22 +12,9 @@ node('maven') {
    	}
    	
    	def version = version()
-   
- //  	stage ('Test SonarQube') {
- //  		sh "curl http://sonarqube:9000/batch/global"
- //  	}
-
-   
-   	stage ('Test and Analisys') {
-   		parallel ( 
-   			'Test' : { 
-   				sh "${mvnCmd} test"
-   				step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-   			},
-   			'Static Ananlysis' : {
-   				sh "${mvnCmd} org.jacoco:jacoco-maven-plugin:report sonar:sonar -Dsonar.host.url=http://sonarqube:9000/ -DskipTests=true"
-   			}   			
-   		)
+      
+   	stage ('Static Ananlysis') {
+   		sh "${mvnCmd} org.jacoco:jacoco-maven-plugin:report sonar:sonar -Dsonar.host.url=http://sonarqube:9000/ -DskipTests=true"
    	}
    
  //  	stage ('Push to Nexus') {
