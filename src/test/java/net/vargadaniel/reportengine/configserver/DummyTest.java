9fa7c1eceb61dbc456f526f3c03272f30ded08d3
vargadan <danielvrg@gmail.com>
2017-09-18 22:17:13 +0200

    test

---
+++ src/test/java/net/vargadaniel/reportengine/configserver/DummyTest.java
@@ -0,0 +1,14 @@
+package net.vargadaniel.reportengine.configserver;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyTest {
+	
+	@Test
+	public void testTrue() {
+		assertTrue(true);
+	}
+
+}
